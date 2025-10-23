527fa0fa876742cc7f8da931e7c770cf2e1d6424
8kJordan <thepro321leyskil@gmail.com>
2025-10-23 04:49:16 -0400

    Add database user Schema & update loginAttempt schema
    Change: Add user schema through mongoose's schema interface. Update
    loginAttempt schema to represent user as email instead of an arbitrary
    string of characters
    
    Why?: To allow for seamless connections and proper requirements of user
    data before sending it to mongo. As well as determining that emails will
    be our users usernames in the app
    
    Authored By: Jordan Sanchez

--- backend/src/schemas/auth.ts
+++ backend/src/schemas/auth.ts
@@ -8,19 +8,11 @@ const passwordSchema = zod.string()
     .regex(/[0-9]/, "Password must include at least one number")
     .regex(/[@$!%*?&]/, "Password must include at least one special character");
 
-const usernameSchema = zod.string()
-    .min(6, "Username must be at least 6 characters long")
-    .max(20, "Username must not exceed 20 characters")
-    .regex(/^[a-zA-Z0-9_-]+$/, "Username can only contain letters, numbers, hyphens, and underscores")
-    .refine((value) => !/^\d+$/.test(value), {
-        message: "Username cannot be only numbers",
-    })
-    .refine((value) => !/[@$!%*?&]/.test(value), {
-        message: "Username cannot contain special characters like @$!%*?&",
-    });
+const emailSchema = zod
+    .email()
 
 export const loginAttempt = zod.object({
-    username: usernameSchema,
+    email: emailSchema,
     password: passwordSchema,
 })
 
