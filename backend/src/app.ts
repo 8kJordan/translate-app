cb89d1925fee471e7f35a7b5f09cbe7937af4a0d
8kJordan <thepro321leyskil@gmail.com>
2025-10-23 04:53:31 -0400

    Add database availability handling logic
    Change: Return 500 errors if database connection is down when API is hit
    
    Why?: To prevent error stack traces from being exposed in the case that
    the DB is down
    
    Authored By: Jordan Sanchez

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,27 @@
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import apiRouter from "@routes/index";
+import { isDbAvailable } from "@db/connection";
 
 const app: Application = express();
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use("/api", apiRouter);
+app.use("/api", (req, res, next) => {
+    if (!isDbAvailable()) {
+        res.status(500).json({
+            "status": "error",
+            "errType": "ServerError"
+        });
+    }
+    next();
+}, apiRouter);
 
-// 404 fallback
+// 404 fallback in case no routes are matched
 app.use((_req, res) => {
     res.status(404).json({ error: "Not Found" });
 });
