86fa474431235fce6a1073ba8158aab63c3404ee
8kJordan <thepro321leyskil@gmail.com>
2025-10-25 14:44:16 -0400

    Add expiration if not registered logic
    Change: Add an expiration timeout to User objects if they dont register
    their email within the alotted time of 24 hours
    
    Why?: Prevents the DB from getting cluttered with users that have not
    verified their accounts after a reasonable period of time
    
    Authored By: Jordan Sanchez

--- backend/src/db/user.model.ts
+++ backend/src/db/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { TIME } from "@utils/constants"
 
 const userSchema = new Schema({
     email: { type: String, required: true, index: true, unique: true },
@@ -8,6 +9,21 @@ const userSchema = new Schema({
     lastName: { type: String, required: true },
     isVerified: { type: Boolean, default: false, required: true },
     refreshToken: { type: String, default: null },
-})
+    },
+    {timestamps: true})
+
+// expire db entry in 24 hours if email verification was not complete
+if (process.env.NODE_ENV === "production") { // if in prod, user will be wiped in 24 hour
+    userSchema.index(
+        { createdAt: 1},
+        { expireAfterSeconds: TIME.hToS(24), partialFilterExpression: { isVerified: false }}
+    )
+} else{
+    userSchema.index(
+        { createdAt: 1},
+        { expireAfterSeconds: TIME.mToS(5), partialFilterExpression: { isVerified: false }}
+    )
+}
+
 
 export const User = model("User", userSchema);
