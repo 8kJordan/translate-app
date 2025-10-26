5949dcdc9867c69d6c6f4ce92489a88b11adfd5f
8kJordan <thepro321leyskil@gmail.com>
2025-10-26 18:24:29 -0400

    Add more jwt utility functions
    Change: Add utility functions to clear session cookies, sign access and
    refresh tokens independendtly from one another, and to set the access
    and refresh tokens as cookies independently of one another
    
    Why?: To abstract away all JWT functionality to one organized page
    
    Authorized By: Jordan Sanchez

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -17,36 +17,74 @@ function requireEnv(name: string): string {
   return value;
 }
 
-export function generateAuthTokens(userId: string): AuthTokens {
+export function generateAccessToken(userId: string): string {
   const accessSecret = requireEnv("AUTH_SECRET");
   const accessExp = requireEnv("AUTH_SECRET_EXPIRATION");
+
+  return jwt.sign({ userId }, accessSecret, {
+    expiresIn: accessExp as any,
+  });
+}
+
+export function generateRefreshToken(userId: string): string {
   const refreshSecret = requireEnv("AUTH_REFRESH_SECRET");
   const refreshExp = requireEnv("AUTH_REFRESH_EXPIRATION");
 
-  const accessToken = jwt.sign({ userId }, accessSecret, {
-    expiresIn: accessExp as any,
-  });
-
-  const refreshToken = jwt.sign({ userId }, refreshSecret, {
+  return jwt.sign({ userId }, refreshSecret, {
     expiresIn: refreshExp as any,
   });
+}
+
+export function generateAuthTokens(userId: string): AuthTokens {
+  const accessToken: string = generateAccessToken(userId);
+  const refreshToken: string = generateRefreshToken(userId);
 
   return { accessToken, refreshToken };
 }
 
-export function setAuthCookies(res: Response, accessToken: string, refreshToken: string): void {
+// functions to attach access and refresh tokens to response cookies
+
+export function setAccessToken(res: Response, accessToken: string): void {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true or false depending on .env
     maxAge: TIME.mToMs(15), // 15 minutes in milliseconds
     sameSite: "strict", // superficial CSRF protection, need csrf tokens
+    path: "/api/"
   });
+}
 
+export function setRefreshToken(res: Response, refreshToken: string): void {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true or false depending on .env
-    maxAge: TIME.hToMs(24),  // 24 hours is milliseconds
-    sameSite: "strict", // superficial CSRF protection, need csrf tokens
+    maxAge: TIME.dToMs(7),  // 7 days is milliseconds
+    sameSite: "strict", // superficial CSRF protection, need csrf tokensa
+    path: "/api/auth/refresh"
   });
 }
+
+export function setAuthCookies(res: Response, accessToken: string, refreshToken: string): void {
+  setAccessToken(res, accessToken);
+  setRefreshToken(res, refreshToken);
+}
+
+// function to clear all session cookies, used to successfully logout a user
+export function clearSessionCookies(res: Response): void {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/api/"
+  });
+
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/api/auth/refresh"
+  });
+}
+
+
 
