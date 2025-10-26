ea27f0d38853e0c7fef98cb2dc9eac2557b80fa1
8kJordan <thepro321leyskil@gmail.com>
2025-10-26 18:40:46 -0400

    Add /refresh and /logout routes
    Change: Add both /refresh and logout endpoints
    
    Why?: Both of these endpoints are necessary to complete the
    authentication API workflow, refresh provisions a new access token and
    logout clears user cookie session
    
    Authored By: Jordan Sanchez

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
-import { login, register, verifyEmail} from "@controllers/auth";
+import { login, register, verifyEmail, refreshAccessToken, logout} from "@controllers/auth";
 
 const router = Router();
 
 router.post("/", login); // auth route
 router.post("/register", register); // register route
 router.get("/verify/:token", verifyEmail); // verification route
+router.post("/refresh", refreshAccessToken); // refresh access token route
+router.get("/logout", logout);
 
 export default router;
