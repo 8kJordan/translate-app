8fb951699ad34e6dc5a64606939d46ae1bf8438c
8kJordan <thepro321leyskil@gmail.com>
2025-11-06 01:59:36 -0500

    User Registration Redirection Logic Complete
    Change: Add redirection logic for when a user successfully verifies
    their email instead of returning plain json. Add custom HTML views as
    redirection messages
    
    Why?: To allow the front end to take control of the registered user once
    they successfully register
    
    Authored By: Jordan Sanchez

---
+++ backend/src/utils/redirectTemplate.ts
@@ -0,0 +1,17 @@
+
+export const redirectTemplate: string = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta http-equiv="refresh" content="2;url=/" />
+            <title>Email Verified</title>
+        </head>
+        <body>
+            <h2>✅ Email successfully verified!</h2>
+            <p>Redirecting to dashboard...</p>
+            <p>
+            Not redirected yet? Click <a href="group9-contacts.com/">Here</a>
+            </p>
+        </body>
+        </html>
+`
